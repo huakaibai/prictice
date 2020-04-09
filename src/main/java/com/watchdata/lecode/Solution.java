f678fb102baa34910842a496a810f9c0ff49e994
zhibin.wang <709129286@QQ.COM>
2020-04-09 09:03:21 +0800

    java spi

--- src/main/java/com/watchdata/lecode/Solution.java
+++ src/main/java/com/watchdata/lecode/Solution.java
@@ -137,4 +137,29 @@ return 1;
         stack.forEach(c->stringBuilder.append(c));
         System.out.println(stringBuilder.toString());*/
     }
+
+    /**
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param s
+     * @param n
+     * @return
+     */
+    public String reverseLeftWords(String s, int n) {
+        char[] chars = s.toCharArray();
+        char[] dest = new char[chars.length];
+        System.arraycopy(chars,n,dest,0,chars.length-n);
+        System.arraycopy(chars,0,dest,chars.length-n,n);
+        System.out.println(dest);
+        return String.valueOf(dest);
+    }
+
+    @Test
+    public void testReverseLeftWords(){
+        String abcdefg = reverseLeftWords("abcdefgdf", 3);
+        System.out.println(abcdefg);
+    }
 }
